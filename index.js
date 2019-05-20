6d3ad62059e06065e100163915668c5684cbcf5e
sarpisik <contact@sarpisik.com>
2019-05-20 13:59:27 +0200

    Edit UI

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let sizeButton = document.getElementById('change-size'),
   clearButton = document.getElementById('clear'),
   container = document.querySelector('.row.justify-center-row');
 
+// Initialize application
 window.addEventListener('load', initialApp);
 
 function initialApp() {
@@ -29,6 +30,7 @@ function renderTable(size) {
   container.appendChild(table);
 }
 
+// Top class
 class App {
   constructor(classNames, elementType = 'div') {
     this.createElementWithClass(classNames, elementType);
@@ -46,26 +48,40 @@ class App {
     }
   }
 }
-
+// Parent of all the rows on a table.
 class Table extends App {
   constructor(rowCount) {
     super('table row column');
     super.renderChildren(rowCount, Row);
   }
 }
+// Parent of all the squares on a line
 class Row extends App {
   constructor(squareCount) {
     super('f-1 row');
     super.renderChildren(squareCount, Square);
   }
 }
+// A child square element in a row.
 class Square extends App {
   constructor() {
     super('f-1 square');
+    // Initial white background color
+    this.red = 255;
+    this.green = 255;
+    this.blue = 255;
+
+    // Set new background color by user choice
     this.colorStyles = {
-      0: 'rgb(0,0,0)',
-      1: this.generateRandomColor()
+      0: this.setBlackWhiteColor.bind(this),
+      1: this.getRandomColor.bind(this),
+      2: this.setColorOnRepeat.bind(this)
     };
+
+    // Set initial background color
+    this.changeBackgroundColor(this.setRgbColor());
+
+    // Change background on mouse hover
     this.element.addEventListener(
       'mouseenter',
       this.handleBackgroundChange.bind(this)
@@ -73,31 +89,55 @@ class Square extends App {
   }
 
   handleBackgroundChange() {
-    const color = this.colorStyles[colorStyle];
+    const colorHandler = this.colorStyles[colorStyle];
+    const color = colorHandler();
     this.changeBackgroundColor(color);
   }
 
   changeBackgroundColor(color) {
     this.element.style.backgroundColor = color;
-    // this.element.removeEventListener('mouseenter', this.changeBackgroundColor);
   }
 
-  generateRandomColor() {
-    let red = this.getRandomNumber();
-    let green = this.getRandomNumber();
-    let blue = this.getRandomNumber();
-    return `rgb(${red},${green},${blue})`;
+  setBlackWhiteColor() {
+    this.red = 0;
+    this.green = 0;
+    this.blue = 0;
+    return this.setRgbColor();
+  }
+
+  getRandomColor() {
+    this.red = this.getRandomNumber();
+    this.green = this.getRandomNumber();
+    this.blue = this.getRandomNumber();
+    return this.setRgbColor();
+  }
+
+  setColorOnRepeat() {
+    this.red = Math.floor(this.red * 0.9);
+    this.green = Math.floor(this.green * 0.9);
+    this.blue = Math.floor(this.blue * 0.9);
+    return this.setRgbColor();
   }
 
   getRandomNumber() {
     return Math.floor(Math.random() * 255);
   }
+
+  setRgbColor() {
+    return `rgb(${this.red},${this.green},${this.blue})`;
+  }
 }
 
-// Event handlers
+/*
+ * Event Handlers
+ */
+
 function handleSizeRequest() {
-  const answer = getRequestedSize();
+  const answer = requestNewSize();
+  // If user click on cancel, end request.
   if (answer === null) return;
+
+  // If the answer is not a valid number, make request again.
   const sizeNumber = Number(answer);
   if (!sizeNumber || sizeNumber < 1 || sizeNumber > 100) {
     alert(`${sizeNumber} is not a valid number. Please try again.`);
@@ -114,13 +154,12 @@ function handleColorStyleSelection({ target }) {
 function handleClearTable() {
   createNewTable(size);
 }
-function changeBackgroundColor() {
-  this.style.backgroundColor = '#000';
-  this.removeEventListener('mouseenter', changeBackgroundColor);
-}
 
-// Helpers
-function getRequestedSize() {
+/*
+ * Helpers
+ */
+
+function requestNewSize() {
   return prompt(
     'Enter the number of squares in a range of 1 to 100 to change the table size. E.g. 10 means 10*10.'
   );
